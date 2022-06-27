d99e31789360959f98dcbfc84281404d333a5766
Prasundas99 <prasundas115@gmail.com>
2022-06-27 18:59:26 +0000

    feat: project references

--- ServiceBusIntegrationDemo/SBReciver/Model/PersonModel.cs
+++ ServiceBusIntegrationDemo/SBReciver/Model/PersonModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,12 @@ using System.Threading.Tasks;
 namespace SBReciver.Model
 {
     internal class PersonModel
+    
     {
+        [Required]
+        public string FirestName { get; set; }
+       
+        [Required]
+        public string LastName { get; set; }
     }
 }
